89bd51181b1effd69773cbcea8277ad3a54028d7
王朋 <wangpeng@sensorsdata.cn>
2021-02-07 19:42:17 +0800

    add

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const request = require('request');
 var bookFoot = {
     "msgtype": "text",
     "text": {
-        "content": "【时间：15:30:00】朋友们，别忘了预定晚餐哦!",
+        "content": "【15:30:00】朋友们，别忘了预订晚餐哦!",
+        "mentioned_list": ["@all"]
+    }
+}
+
+var footCome = {
+    "msgtype": "text",
+    "text": {
+        "content": "【18:00:00】朋友们，晚餐到了，开始干饭喽!",
         "mentioned_list": ["@all"]
     }
 }
@@ -12,15 +20,23 @@ var bookFoot = {
 var bookCar = {
     "msgtype": "text",
     "text": {
-        "content": "【时间：20:50:00】朋友们，加班辛苦了，可以打车啦！",
+        "content": "【20:50:00】朋友们，加班辛苦了，可以打车啦！",
         "mentioned_list": ["@all"]
     }
 }
 
-function requestfun(contnet) {
+//内部
+var test1 = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=d1af7337-d0b7-46db-abeb-cbd7acbf8db2"
+//大群
+var test2 = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=8ea533b5-0991-4daa-b0e0-a9e4081cdf69"
+
+function requestfun(contnet, webhookUrl) {
     // url 为企业机器人的webhook
+
+    // test1: https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=d1af7337-d0b7-46db-abeb-cbd7acbf8db2
+    // test2: https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=8ea533b5-0991-4daa-b0e0-a9e4081cdf69
     request({
-        url: "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=425f9fea-7e99-4c52-b172-1bda7f62c09c",
+        url: webhookUrl,
         method: "POST",
         headers: {
             "content-type": "application/json",
@@ -33,15 +49,24 @@ function requestfun(contnet) {
 
 const scheduleCronstyle = () => {
     //每分钟的第30秒定时执行一次:
-    schedule.scheduleJob('00 30 15 * * *', () => {
+    schedule.scheduleJob('00 30 15 * * 1-6', () => {
         console.log('点餐:' + new Date());
-        requestfun(bookFoot);
+        requestfun(bookFoot, test1);
+        requestfun(bookFoot, test2);
     });
 
-    schedule.scheduleJob('00 50 20 * * *', () => {
-        console.log('打车:' + new Date());
-        requestfun(bookCar);
+    schedule.scheduleJob('00 00 18 * * 1-6', () => {
+        console.log('饭来了:' + new Date());
+        requestfun(footCome, test1);
+        requestfun(footCome, test2);
     });
+
+    schedule.scheduleJob('00 50 20 * * 1-6', () => {
+        console.log('打车了:' + new Date());
+        requestfun(bookCar, test1);
+    });
+
+    
 }
 
 scheduleCronstyle();
