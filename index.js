77e51d88bf3d5fa661e8399c4ce7412cad064d09
王朋 <wangpeng@sensorsdata.cn>
2021-04-16 18:56:43 +0800

    add

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 const schedule = require("node-schedule");
 const request = require('request');
 const imgList = [
-    'https://wx3.sinaimg.cn/mw690/005Ozs6vgy1gple2b38rbj31gh0u04qp.jpg',
-    'https://wx4.sinaimg.cn/mw690/005Ozs6vgy1gple26tqe7j30g00a0dg0.jpg',
-    'https://wx1.sinaimg.cn/mw690/005Ozs6vgy1gple22i4bnj30tf0icwge.jpg',
-    'https://wx3.sinaimg.cn/mw690/005Ozs6vgy1gple1y6an8j30pa0gu75x.jpg',
-    'https://wx4.sinaimg.cn/mw690/005Ozs6vgy1gple0wf38tj30dw08pdg3.jpg',
-    'https://wx1.sinaimg.cn/mw690/005Ozs6vgy1gple0sac7rj30dw08p0sv.jpg',
-    'https://wx3.sinaimg.cn/mw690/005Ozs6vgy1gple0o9dutj30dw08p74g.jpg',
-    'https://wx4.sinaimg.cn/mw690/005Ozs6vgy1gple0ka44xj30dw08p3yx.jpg',
-    'https://wx3.sinaimg.cn/mw690/005Ozs6vgy1gple0f4ihqj30hs0do752.jpg',
-    'https://wx3.sinaimg.cn/mw690/005Ozs6vgy1gplexoq8e8j30rs0fmabg.jpg'
+    {
+        url: 'https://wx2.sinaimg.cn/mw690/005Ozs6vgy1gplrl1u1fej30u00l0tce.jpg',
+        origin: 'https://wx4.sinaimg.cn/mw690/005Ozs6vgy1gplodbqtalj30u0140x23.jpg'
+    },
+    {
+        url: 'https://wx3.sinaimg.cn/mw690/005Ozs6vgy1gplrwni3ewj30if0ajtnm.jpg',
+        origin: 'https://wx4.sinaimg.cn/mw690/005Ozs6vgy1gplod76f2yj30b40gyaqb.jpg'
+    }
 ]
 
 const getImg = function () {
-    const index = parseInt(Math.random() * 11);
-    return imgList[index]
+    const index = parseInt(Math.random() * 2);
+    const {
+        url,
+        origin
+    } = imgList[index]
+    return {
+        url,
+        origin
+    }
 }
 
-var atAll = {
+const atAll = {
     "msgtype": "text",
     "text": {
         "content": "",
@@ -26,24 +31,31 @@ var atAll = {
     }
 }
 
+function bookFoot() {
+    const {
+        url,
+        origin
+    } = getImg()
 
-var bookFoot = {
-    msgtype: "news",
-    news: {
-        articles: [
-            {
-                title: "别忘了预订晚餐哦!",
-                url: "weixin://dl",
-                picurl: getImg()
-            }
-        ]
+    var bookFoot = {
+        msgtype: "news",
+        news: {
+            articles: [
+                {
+                    title: "别忘了预订晚餐哦!",
+                    url: origin,
+                    picurl: url
+                }
+            ]
+        }
     }
+    return bookFoot
 }
 
 //内部
-var test = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=d1af7337-d0b7-46db-abeb-cbd7acbf8db2"
+const test1 = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=d1af7337-d0b7-46db-abeb-cbd7acbf8db2"
 //大群
-var prod = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=87d95ac8-497a-4152-b3c0-689ce5108268"
+const prod = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=87d95ac8-497a-4152-b3c0-689ce5108268"
 
 function requestfun(contnet, webhookUrl) {
     // url 为企业机器人的webhook
@@ -60,13 +72,13 @@ function requestfun(contnet, webhookUrl) {
 }
 
 
-var diancan = '00 30 15 * * 1-6'
+const diancan = '00 30 15 * * 1-6';
 
 const scheduleCronstyle = () => {
     schedule.scheduleJob(diancan, () => {
        // console.log('点餐:' + new Date());
-        requestfun(bookFoot, prod);
-        requestfun(atAll, prod);
+       requestfun(bookFoot(), prod);
+       requestfun(atAll, prod);
     });
 }
 
--- test.js
+++ test.js
@@ -59,3 +59,17 @@ var footCome = {
         ]
     }
 }
+
+
+const imgList = [
+    'https://wx3.sinaimg.cn/mw690/005Ozs6vgy1gple2b38rbj31gh0u04qp.jpg',
+    'https://wx4.sinaimg.cn/mw690/005Ozs6vgy1gple26tqe7j30g00a0dg0.jpg',
+    'https://wx1.sinaimg.cn/mw690/005Ozs6vgy1gple22i4bnj30tf0icwge.jpg',
+    'https://wx3.sinaimg.cn/mw690/005Ozs6vgy1gple1y6an8j30pa0gu75x.jpg',
+    'https://wx4.sinaimg.cn/mw690/005Ozs6vgy1gple0wf38tj30dw08pdg3.jpg',
+    'https://wx1.sinaimg.cn/mw690/005Ozs6vgy1gple0sac7rj30dw08p0sv.jpg',
+    'https://wx3.sinaimg.cn/mw690/005Ozs6vgy1gple0o9dutj30dw08p74g.jpg',
+    'https://wx4.sinaimg.cn/mw690/005Ozs6vgy1gple0ka44xj30dw08p3yx.jpg',
+    'https://wx3.sinaimg.cn/mw690/005Ozs6vgy1gple0f4ihqj30hs0do752.jpg',
+    'https://wx3.sinaimg.cn/mw690/005Ozs6vgy1gplexoq8e8j30rs0fmabg.jpg'
+]
